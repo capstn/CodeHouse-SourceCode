bcb8ed273915173b990054565e4cf48af05deccd
Deus Vult <clarencejoven1456@gmail.com>
2023-03-07 10:48:51 +0800

    Rewrote the code for dialogue
    I rewrote all the code for it to inherit and encapsulate. Making it secure and more manageable in the future.

---
+++ Code House/Assets/Scripts/Characters.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character {
+
+    public class Characters
+    {
+        private string characterName;
+        private byte range;
+        private bool inRange;
+        private bool canDialogue;
+        private bool questGiver;
+        private GameObject player;
+
+        public Characters() {
+            characterName = "Default";
+            range = 0;
+            inRange = false;
+            canDialogue = false;
+            questGiver = false;
+        }
+
+        public string CharacterName {
+            set => characterName = value;
+            get => characterName;
+        }
+
+        public byte Range {
+            set => range = value;
+            get => range;
+        }
+
+        public bool InRange {
+            set => inRange = value;
+            get => inRange;
+        }
+
+        public void setPlayer() {
+            player = GameObject.Find("Player");
+        }
+
+        public Vector3 getPlayerPostion() {
+            return player.transform.position;
+        }
+
+        public GameObject Player {
+            get => player = player;
+        }
+
+    }
+
+}
--- Code House/Assets/Scripts/Dialogue/Dialogue.cs
+++ Code House/Assets/Scripts/Dialogue/Dialogue.cs
@@ -1,114 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
+using Character;
 using UnityEngine;
 using TMPro;
 
-public class Dialogue : MonoBehaviour
-{
-    public Transform player;
-    public GameObject dialogueSystem;
-    public TMP_Text characterNameHolder;
-    public TMP_Text characterDialogueHolder;
+namespace Character.Behaviour.Conversation {
 
-    public float range; // how far can the character detect
-    public string characterName = ""; // character name
-    public string[] text = new string[5]; // string of dialogues
-    private bool dialogueClosed; // if the dialogue is closed
-    private bool canDialogue; // if the player is within reach for conversation
-    private bool dialogue; // if the conversation is ongoing
-    private bool nextDialogue; // if the player clicked next
-    private byte dialogueNum = 0; // for the number of dialogues in the string
-    public byte dialogueMax; // just minus 1 to the total string of dialogues
+        public class Dialogue : MonoBehaviour {
 
-    private void Start() {
+            private Transform playerLocation;
+            public string characterName;
+            public byte range;
+            private bool inDialogue;
 
-        dialogueClose();
+            public GameObject dialogueSystem;
+            public TMP_Text characterNameHolder;
+            public TMP_Text characterDialogueHolder;
 
-    }
+            Characters myCharacter = new Characters();
 
-    private void Update() {
+            private void Start() {
 
-        if (Vector3.Distance(player.position, transform.position) <= range) {
-            
-            canDialogue = true;
-
-        } else {
-
-            canDialogue = false;
-
-        }
-
-        if (canDialogue == true) {
-
-            if (Input.GetKeyDown("f") && dialogue == false) {
-                dialogueClosed = false;
-                setName();
-                setDialogue(dialogueNum);
-                dialogueOpen();
-
-            } else if (Input.GetKeyDown("f") && dialogue == true) {
-
-                dialogueClose();
+                myCharacter.Range = this.range;
+                myCharacter.setPlayer();
 
             }
 
-            if (dialogueClosed == false && Input.GetMouseButtonDown(0)) {
+            private void Update() {
+                
+                if (Vector3.Distance(myCharacter.getPlayerPostion(), transform.position) <= myCharacter.Range) {
 
-                nextDialogue = true;
+                    if (Input.GetKeyDown("f") && this.inDialogue == false) {
 
-                if (nextDialogue == true) {
-                    if (dialogueNum < dialogueMax) {
-                        dialogueNum += 1;
-                        setDialogue(dialogueNum);
-                    } else {
-                        dialogueNum = 0;
-                        dialogueClosed = true;
+                        dialogueOpen();
+
+                    } else if (Input.GetKeyDown("f") && this.inDialogue == true) {
+
                         dialogueClose();
+
                     }
-                } else if (dialogueClosed == true) {
-                    dialogueNum = 0;
+                    
+                } else {
+
+                    dialogueClose();
+
                 }
+                
             }
 
-        } else {
+            private void dialogueOpen() {
 
-            if (dialogueClosed == false) {
-                dialogueClose();
-                dialogueClosed = true;
+                myCharacter.CharacterName = this.characterName;
+                this.characterNameHolder.SetText(myCharacter.CharacterName);
+                this.dialogueSystem.SetActive(true);
+                this.inDialogue = true;
+
             }
 
-        }
+            private void dialogueClose() {
+
+                this.dialogueSystem.SetActive(false);
+                this.inDialogue = false;
+
+            }
 
     }
 
-    private void setName() {
-        characterNameHolder.SetText(characterName);
-    }
-
-    private void setDialogue(byte dialogueNum) {
-        characterDialogueHolder.SetText(text[dialogueNum]);
-    } 
-
-    public void dialogueOpen() {
-
-        this.dialogue = true;
-
-        if (this.dialogue == true) {
-
-            dialogueSystem.SetActive(true);
-
-        }
-    }
-
-    public void dialogueClose() {
-
-        this.dialogue = false;
-
-        if (this.dialogue == false) {
-
-            dialogueSystem.SetActive(false);
-
-        }
-    }
-    
 }
+
